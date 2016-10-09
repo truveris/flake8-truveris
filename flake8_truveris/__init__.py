c4ac6e99b827aaf27542728a956cbba95c5c6aa6
Chris NeJame <cnejame@truveris.com>
2016-10-08 21:28:56 -0400

    clarified comment

--- flake8_truveris/__init__.py
+++ flake8_truveris/__init__.py
@@ -143,9 +143,8 @@ class CheckTruveris(object):
         context_uses_commas = False
         if opening_bracket != "(":
             # the only times trailing commas shouldn't be used inside a context
-            # are inside a list/tuple/dict comprehension, an if/elif series of
-            # conditions wrapped in parenthesis, and when a long string is
-            # broken up over multiple lines inside parenthesis
+            # are inside a list/tuple/dict comprehension, and when code is
+            # broken up over multipe lines using parenthesis
             context_uses_commas = True
 
         index = context_start_index + 1
