158f9e2cac2a2f7fb963a1063a5c28b4f02550c9
Chris NeJame <cnejame@truveris.com>
2016-10-13 14:11:48 -0400

    added download url

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 requires = [
     "flake8",
 ]
+__pkg_version__ = "0.1"
 
 setup(
     name="flake8_truveris",
-    version="0.1",
+    version=__pkg_version__,
     description=(
         "Flake8 extension for checking Python "
         "code against Truveris's code style guide"
@@ -14,6 +15,10 @@ setup(
     author="Truveris Inc.",
     author_email="engineering@truveris.com",
     url="http://github.com/truveris/flake8-truveris",
+    download_url=(
+        "https://github.com/truveris/flake8-truveris/tarball/{}"
+        .format(__pkg_version__)
+    )
     install_requires=requires,
     packages=['flake8_truveris'],
     entry_points={
